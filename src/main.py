c36b2371affca2203f34831a7c3d232c67ed7a1d
ISRAT510 <isratjahaneva563@gmail.com>
2026-05-02 00:33:12 +0600

    Add basic calculator functions

--- src/main.py
+++ src/main.py
@@ -4,3 +4,8 @@ print("Name: Israt")
 
 today = datetime.date.today()
 print("Today's date:", today)
+
+from utils import add, subtract
+
+print("Addition:", add(5, 3))
+print("Subtraction:", subtract(5, 3))
