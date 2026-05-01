4bf64f8e6db9f63376baf15e883a4f9276a321f1
ISRAT510 <isratjahaneva563@gmail.com>
2026-05-02 00:40:16 +0600

    Add multiply function

--- src/main.py
+++ src/main.py
@@ -5,7 +5,8 @@ print("Name: Israt")
 today = datetime.date.today()
 print("Today's date:", today)
 
-from utils import add, subtract
+from utils import add, subtract, multiply
 
 print("Addition:", add(5, 3))
 print("Subtraction:", subtract(5, 3))
+print("Multiplication:", multiply(5, 3))
